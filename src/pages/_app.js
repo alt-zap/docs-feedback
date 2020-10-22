2e57bb02862bbd275694b24314b3622805ce93c1
Diego Aquino <diego.cruz001@gmail.com>
2020-10-22 15:04:46 -0300

    Initialize Next.js project and add dev configs

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+import '../styles/globals.css';
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
